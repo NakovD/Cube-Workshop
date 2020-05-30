ad410700efb6ebe9aff8da9a11ac07e6548414c7
DavidNakov <nakov_david@mail.bg>
2020-05-30 11:51:28 +0300

    project initialization

---
+++ config/express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+module.exports = (app) => {
+
+    app.engine('.hbs', handlebars({
+        extname: '.hbs'
+    }));
+    app.set('view engine', '.hbs');
+
+    app.use('/static', express.static('public'));
+
+};
